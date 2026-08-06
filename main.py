c13de731affbfc1977bd2be565f5bd506866e2f4
aaravm95 <aaravamudhan@outlook.com>
2026-08-05 21:46:23 -0700

    made it cleaner and made it print enemy and player health after enemy attack

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 
 
+
 def phase2():
     print("------------------------------------------------------------------")
     print("You finally get to the control room...")
@@ -8,87 +9,117 @@ def phase2():
     print("You encounter the evil mastermind!")
     print("He says You want to stop me from launching that bomb?!")
     print("Then you have to stop me!!!")
+    print("------------------------------------------------------------------------------")
 
     playerHealth = 100
     mastermindHealth = 100
 
     print("Playerhealth", playerHealth)
     print("evilMastermind health:", mastermindHealth)
+    print("-------------------------------")
     print("The evil mastermind attacks")
-    playerHealth -= random.randint(5, 20)
+    print("-------------------------------")
+    playerHealth -= random.randint(5, 15)
+    print("Playerhealth", playerHealth)
+    print("evilMastermind health:", mastermindHealth)
+    print("------------------------------------------------------------------------------")
 
     while playerHealth > 0 and mastermindHealth > 0:
+
         attack = input("press a to attack:")
 
         if attack == "a":
             mastermindHealth -= random.randint(10, 20)
+            print("-------------------------------")
             print("You have attacked")
+            print("-------------------------------")
             print("Playerhealth", playerHealth)
             print("evilMastermind health:", mastermindHealth)
+            print("------------------------------------------------------------------------------")
 
             if mastermindHealth <= 0:
                 print("Winner: Player!!!")
+                print("------------------------------------------------------------------------------")
+                print("You deactivate the bomb and save the day!!!")
                 break
 
             if random.randint(1, 2) == 1:
+                print("---------------------------------------")
                 print("the evil mastermind has attacked!!!")
+                print("---------------------------------------")
                 playerHealth -= random.randint(20, 30)
                 print("Playerhealth", playerHealth)
+                print("evilMastermind health:", mastermindHealth)
+                print("------------------------------------------------------------------------------")
 
                 if playerHealth <= 0:
                     print("Winner: evil mastermind!!!")
                     print("You lose")
+                    print("------------------------------------------------------------------------------")
+                    print("The bomb explodes and everyone dies. The end...")
                     break
 
 
 def entrance():
+    print("------------------------------------------------------------------------------")
     print("You arrived at the entrance.")
     entrancePathways = input("You see two pathways: right or left: ").lower()
     if entrancePathways == "right" or entrancePathways == "r":
         print("You go right")
         print("you see lots of lazers ahead you")
         print("You also see a keypad")
+        print("------------------------------------------------------------------------------")
         keypad_or_lasers = input("What do you do use the keypad or dodge the lasers?")
         if keypad_or_lasers == "laser" or keypad_or_lasers == "l" or keypad_or_lasers == "d" or keypad_or_lasers == "dodge":
             print("You accidentally touch a laser and alarm goes off. ")
             print("You run out of the building.")
+            print("------------------------------------------------------------------------------")
             lasercon = input("you see a trapdoor and a door that says EXIT. Which one do you go through???")
             if lasercon == "d" or lasercon == "door":
                 print("You go through the door. You see 25 guards waiting to kill you on the other side.")
                 print("you DIED.")
+                print("------------------------------------------------------------------------------")
 
             elif lasercon == "trapdoor" or lasercon == "t":
                 print(
                     "You go through the trapdoor after an hour of walking, you finally walk up and start back at the entrance")
+                print("------------------------------------------------------------------------------")
                 entrance()
         elif keypad_or_lasers == "k" or keypad_or_lasers == "keypad":
             password = input("ENTER THE PASSWORD")
             if password == "510":
+                print("------------------------------------------------------------------------------")
                 phase2()
 
     elif entrancePathways == "left" or entrancePathways == "l":
+        print("------------------------------------------------------------------------------")
         print("You go left...")
         print("You encounter a robot with a gun")
         FightRun = input("Fight or run???")
 
         if FightRun == "f" or FightRun == "fight":
+            print("------------------------------------------------------------------------------")
             print("You rapidly punch the robot")
             print("It eventually breaks")
             dimoansteal = input("you see a diamond do you take it???")
 
             if dimoansteal == "y" or dimoansteal == "yes":
+                print("------------------------------------------------------------------------------")
                 print("It was a trap!!! You fall into an endless room.")
 
             elif dimoansteal == "n" or dimoansteal == "no":
+                print("------------------------------------------------------------------------------")
                 print("You go on without it...")
                 print("You see a door with a keypad you decide to use it...")
                 password1 = input("ENTER THE PASSWORD")
 
                 if password1 == "510":
+                    print("------------------------------------------------------------------------------")
                     print("Door is unlocked")
                     phase2()
 
         elif FightRun == "r" or FightRun == "run":
+            print("------------------------------------------------------------------------------")
             print("You run as fast as you can.The robot follows closely")
             keypad1 = input("You see a keypad.Do you use it???")
 
@@ -119,7 +150,7 @@ def intro():
     print(" -☆☆☆|SPY MISSION 1549|☆☆☆- ")
     print("•-----------•☆☆☆☆☆•----------•")
     print("===========================")
-
+    print("------------------------------------------------------------------------------")
     print("You are a spy trying to stop an evil mastermind from launching a powerful nuclear bomb.")
     print("You must deactivate the bomb before the countdown reaches zero.")
 
@@ -129,12 +160,14 @@ def intro():
     ).lower()
 
     if Choice1 == "s" or Choice1 == "sneak":
+        print("------------------------------------------------------------------------------")
         Choice2 = input(
             "You sneak past the guards, but one gets alerted. "
             "Fight or run? "
         ).lower()
 
         if Choice2 == "f" or Choice2 == "fight":
+            print("------------------------------------------------------------------------------")
             print("You knock the guard out cold. Luckily, none of the other guards noticed.")
             print("one of the drops a paper with the code 510 you might want to remember that...")
             entrance()
@@ -147,6 +180,7 @@ def intro():
             ).lower()
 
             if Choice3 == "left" or Choice3 == "left door" or Choice3 == "l":
+                print("------------------------------------------------------------------------------")
                 print("You smash the left door open just before the guards catch you!")
                 print("as you go through the hallway you see the code 510")
                 entrance()
@@ -155,6 +189,7 @@ def intro():
                 print("You fall into a hidden trap. Game over!")
 
     elif Choice1 == "f" or Choice1 == "fight":
+        print("------------------------------------------------------------------------------")
         print("You try to fight, but they lock you in a cell.")
         print("You see a vent and a rock.")
 
@@ -163,11 +198,13 @@ def intro():
         ).lower()
 
         if Choice4 == "climb" or Choice4 == "c" or Choice4 == "v" or Choice4 == "vent":
+            print("------------------------------------------------------------------------------")
             print("You climb through the vent.")
             print("You forgot an oxygen mask and faint from the toxic fumes.")
             print("Game over!")
 
         elif Choice4 == "throw rock" or Choice4 == "t" or Choice4 == "r":
+            print("------------------------------------------------------------------------------")
             print("You throw the rock.")
             print("The guard gets knocked out. You grab the keys, open the cell, and run!")
             print("The guard drops a paper with the code 510 you might want to remember that...")
